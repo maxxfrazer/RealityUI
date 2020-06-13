a1d50a9e6547b4dccf71e81febca94b7887256a8
Max Cobb <5754073+maxxfrazer@users.noreply.github.com>
2020-06-13 10:44:33 +0100

    move functionality back into HasPivotTouch, this one must be an exception

--- Sources/RealityUI/HasPivotTouch.swift
+++ Sources/RealityUI/HasPivotTouch.swift
@@ -12,6 +12,34 @@ import RealityKit
 /// Hence no documentation yet.
 public protocol HasPivotTouch: HasPanTouch {}
 
+internal extension HasPivotTouch {
+  var collisionPlane: float4x4? {
+    return self.transformMatrix(relativeTo: nil)
+      * float4x4(pivotRotation)
+  }
+
+  func arTouchStarted(_ worldCoordinate: SIMD3<Float>, hasCollided: Bool) {
+    self.lastGlobalPosition = worldCoordinate
+  }
+  func arTouchUpdated(_ worldCoordinate: SIMD3<Float>, hasCollided: Bool) {
+    var localPos = self.convert(position: worldCoordinate, from: nil)
+    localPos = self.pivotRotation.act(localPos)
+    var lastLocalPos = self.convert(position: self.lastGlobalPosition, from: nil)
+    lastLocalPos = self.pivotRotation.act(lastLocalPos)
+    let lastAngle = atan2f(lastLocalPos.x, lastLocalPos.z)
+    let angle = atan2f(localPos.x, localPos.z)
+
+    self.orientation *= simd_quatf(angle: angle - lastAngle, axis: self.pivotAxis)
+    self.lastGlobalPosition = worldCoordinate
+  }
+  func arTouchCancelled() {
+    self.arTouchEnded(nil)
+  }
+  func arTouchEnded(_ worldCoordinate: SIMD3<Float>?) {
+    self.lastGlobalPosition = .zero
+  }
+}
+
 public struct PivotComponent: Component {
   internal var lastTouchAngle: Float?
   internal var lastGlobalPosition: SIMD3<Float> = .zero

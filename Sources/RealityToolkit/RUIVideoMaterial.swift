2c64cd23faad14dc7e76e9d0b417705718bd50f3
Max Cobb <maxxc@mac.com>
2022-05-15 11:05:14 +0100

    added documentation for all the methods and properties of RUIVideoMaterial. Updated setVideoState method.

--- Sources/RealityToolkit/RUIVideoMaterial.swift
+++ Sources/RealityToolkit/RUIVideoMaterial.swift
@@ -9,10 +9,17 @@ import RealityKit
 import AVKit
 
 @available(iOS 14.0, macOS 11.0, *)
+/// RealityToolkit's RUIVideoMaterial helps you create a RealityKit VideoMaterial 🎥🎞
 public class RUIVideoMaterial {
 
-    private var playing = false
-    public let useLooper: Bool
+    /// If the video is currently playing.
+    public var playing: Bool { self.videoState == .playing }
+
+    /// State of the video.
+    public private(set) var videoState: VideoState = .notInitialized
+    /// Whether the video will loop.
+    public let loops: Bool
+    /// Source of the video file or stream.
     public let videoSource: VideoSource
 
     private var player: AVPlayer?
@@ -20,16 +27,8 @@ public class RUIVideoMaterial {
     private var playerItem: AVPlayerItem?
     private var playerLooper: AVPlayerLooper?
 
+    /// Video material that can be added to a model in RealityKit
     public private(set) var videoMaterial: VideoMaterial?
-
-    private func createAVPlayer(_ named: String, withExtension ext: String) -> AVPlayer? {
-        guard let url = Bundle.main.url(forResource: named, withExtension: ext) else {
-            return nil
-        }
-
-        let avPlayer = AVPlayer(url: url)
-        return avPlayer
-    }
 
     private func createVideoPlayerItem() -> AVPlayerItem? {
         switch self.videoSource {
@@ -45,13 +44,20 @@ public class RUIVideoMaterial {
         }
     }
 
+    /// Source of the video for AVPlayer
     public enum VideoSource {
+        /// URL of the local or remote video file or stream.
         case url(URL)
+        /// Name and extension of a video file found in your app's bundle.
         case file(named: String, ext: String)
     }
 
-    public init?(videoSource: VideoSource, useLooper: Bool = false) {
-        self.useLooper = useLooper
+    /// Create an RUIVideoMaterial, a wrapper for VideoMaterials in RealityKit.
+    /// - Parameters:
+    ///   - videoSource: Source of the video, either a local file, or a URL.
+    ///   - loops: Whether the video should loop.
+    public init?(videoSource: VideoSource, loops: Bool = false) {
+        self.loops = loops
         self.videoSource = videoSource
         if !self.preparePlayer() {
             return nil
@@ -60,7 +66,7 @@ public class RUIVideoMaterial {
 
     fileprivate func preparePlayer() -> Bool {
         let playerItem = createVideoPlayerItem()
-        if useLooper {
+        if loops {
             guard let avPlayerItem = playerItem else { return false }
             let queuePlayer = AVQueuePlayer()
             self.playerLooper = AVPlayerLooper(player: queuePlayer, templateItem: avPlayerItem)
@@ -74,15 +80,18 @@ public class RUIVideoMaterial {
         avPlayer.actionAtItemEnd = .pause
         avPlayer.pause()
         self.videoMaterial = VideoMaterial(avPlayer: avPlayer)
+        self.videoState = .ready
         return true
     }
 
-    public func reset() {
-        guard let avPlayer = self.player else { return }
-        // reset state is paused at the beginning of the video
+    /// Set video back to the start and pause.
+    /// - Returns: Returns false if method fails.
+    public func reset() -> Bool {
+        guard let avPlayer = self.player else { return false }
         avPlayer.pause()
         avPlayer.seek(to: .zero)
-        self.playing = false
+        self.videoState = .ready
+        return true
     }
 
     public func sceneUpdate() {
@@ -93,13 +102,29 @@ public class RUIVideoMaterial {
         }
     }
 
-    public func enablePlayPause(_ enable: Bool) {
-        self.playing = enable
-        guard let avPlayer = self.player else { return }
-        if self.playing, avPlayer.timeControlStatus == .paused {
+    /// State of the AVPlayer and video.
+    public enum VideoState {
+        /// Video is not initialised, either it is being fetched still, or something is wrong with the video source.
+        case notInitialized
+        /// Video is at the beginning and ready to play.
+        case ready
+        /// Video is paused.
+        case paused
+        /// Video is playing.
+        case playing
+    }
+
+    /// Set the state of the video, between playing and paused.
+    /// - Parameter playing: Add `true` to play, `false` to pause.
+    /// - Returns: Returns false if method fails.
+    public func setVideoState(playing: Bool) -> Bool {
+        guard let avPlayer = self.player, self.videoState != .notInitialized else { return false }
+        self.videoState = playing ? .playing : .paused
+        if self.videoState == .playing, avPlayer.timeControlStatus == .paused {
             avPlayer.play()
-        } else if !self.playing, avPlayer.timeControlStatus != .paused {
+        } else if self.videoState == .paused, avPlayer.timeControlStatus != .paused {
             avPlayer.pause()
         }
+        return true
     }
 }
